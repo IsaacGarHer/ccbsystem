3d22fdfdbeaf00fedb264b3a0c06bc713fb81eeb
IsaacGH <garciaisaac468@gmail.com>
2020-01-05 14:12:11 -0600

    add routes to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+import * as ROUTES from './global/routes'
+
 export default ( ) => (
   <Fragment>
     <Switch>
