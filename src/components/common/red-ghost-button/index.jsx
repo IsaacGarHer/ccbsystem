8f06ea6c81ac62853505dbf007c4c3821c38f3ad
IsaacGH <garciaisaac468@gmail.com>
2020-01-06 19:26:55 -0600

    create red ghost button component

---
+++ src/components/common/red-ghost-button/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './index.scss'
+
+export default props => (
+  <button
+    onClick = { ( ) => props.function ? props.function : null }
+    className = 'red-ghost-button'
+    id = { props.id ? props.id : null } >{ props.text ? props.text : null }</button>
+)
